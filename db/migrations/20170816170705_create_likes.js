73a66444e30b44da065e9d3b00bdf777d1b8c4ab
agault <asialevesquegault@gmail.com>
2017-08-16 17:09:46 +0000

    added migration likes

--- db/migrations/20170816170705_create_likes.js
+++ db/migrations/20170816170705_create_likes.js
@@ -1,15 +1,12 @@
-
 exports.up = function(knex, Promise) {
-  return knex.schema.createTable('comments', function (table) {
-    table.increments()
-    table.string('content')
+  return knex.schema.createTable('likes', function (table) {
     table.integer('user_id')
     table.foreign('user_id').references('users.id')
     table.integer('url_id')
     table.foreign('url_id').references('URLs.id')
-    table.integer('rating')
+
   })
 }
 exports.down = function(knex, Promise) {
-  return knex.schema.dropTable('comments')
+  return knex.schema.dropTable('likes')
 };
