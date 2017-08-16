a3fb1f0ecaefcfe78d6abf8936cee80c8f89291b
agault <asialevesquegault@gmail.com>
2017-08-16 22:55:21 +0000

    added login feature

--- routes/logs.js
+++ routes/logs.js
@@ -21,8 +21,21 @@ module.exports = (knex) => {
   router.post("/login", (req, res) => {
     console.log(req.body.email, req.body.password)
     // for now donot check the credentials! Just login!
-    req.session.email = req.body.email
-    res.redirect("/")
+    userDatahelpers.getUser('email',req.body.email,(err, user)=>{
+      if (err){
+        return res.send('database connection error')
+      }
+      if (user.length === 0){
+        return res.send('email is not registered')
+      }
+      else if (!bcrypt.compareSync(user.password, req.body.password)) {
+        return res.send('invalid password')
+      } else {
+        req.session.email = req.body.email
+        res.redirect("/")
+      }
+    })
+
   })
 
   // Registration
