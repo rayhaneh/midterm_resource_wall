adc8746ab90c333c46715c25ff8b9b437a89e564
Rayhaneh Banyassady <r.b.assady@gmail.com>
2017-08-17 12:06:09 +0000

    Added the /urls route to the server

--- routes/urls.js
+++ routes/urls.js
@@ -6,6 +6,16 @@ const router         = express.Router()
 
 module.exports = (urlDataHelpers) => {
 
+
+  router.get('/', (req, res) => {
+    urlDataHelpers.getURLs((err, urls) => {
+      if (err) {
+        return res.send('Error while connecting to the database.')
+      }
+      res.render('show_urls', {'urls': urls})
+    })
+  })
+
   //
   router.get('/:id', (req, res) => {
     urlDataHelpers.getURL(1, (err, url) => {
