5cad30c075e8c98e6cb6a3910440cfbf9e6b4af1
Rayhaneh Banyassady <r.b.assady@gmail.com>
2017-08-17 17:24:45 +0000

    Added the first version of the getOverallRating function

--- db/data_helpers/url_data_helpers.js
+++ db/data_helpers/url_data_helpers.js
@@ -35,9 +35,18 @@ module.exports = function makeURLDataHelpers(knex) {
     // --- FUNCTIONS THAT WE NEED:
     // GET ALL RATINGS FOR A URL AND RETURNS THE OVERAL RATING
     getOveralRating: function(id, callback) {
+      // --- FINISH THIS FUNCTION LATER ---
       // knex
       // .select('rating')
-      // .from('')
+      // .from('comments')
+      // .where('url_id', '=', id)
+      // .then((ratings) => {
+      //   console.log(ratings)
+      //   return callback(null, 3)
+      // })
+      // .cathc((err) => {
+      //   return callback(err)
+      // })
     },
 
     // GET ALL COMMENTS FOR ONE URL
