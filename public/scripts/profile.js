7c17cd7cfa5e67f268987cecee87c3d24b647c31
Rayhaneh Banyassady <r.b.assady@gmail.com>
2017-08-17 20:21:41 +0000

    Updated ajax to accept category

--- public/scripts/profile.js
+++ public/scripts/profile.js
@@ -1,10 +1,16 @@
 $(document).ready(function() {
 
   $('#newForm').on('submit', function(event) {
+    console.log($(this).serializeArray()[0].value)
+    console.log($(this).serializeArray()[1].value)
+    console.log($(this).serializeArray()[2].value)
+    console.log($(this).serializeArray()[3].value)
+
     let newURL = {
       URL           : $(this).serializeArray()[0].value,
       Title         : $(this).serializeArray()[1].value,
-      Desc          : $(this).serializeArray()[0].value,
+      Desc          : $(this).serializeArray()[2].value,
+      cat_id        : $(this).serializeArray()[3].value,
       overallRating : 0
     }
     event.preventDefault()
