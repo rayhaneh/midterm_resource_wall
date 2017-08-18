bb44f7f643ec44c5f63ac6fec29470db49f181c8
Rayhaneh Banyassady <r.b.assady@gmail.com>
2017-08-18 10:26:31 +0000

    Cleaned up all ejs files.

---
+++ public/scripts/formValidation.js
@@ -0,0 +1,19 @@
+function validateRegisteration(data) {
+  // to be added later
+  return true
+}
+
+
+
+function validateLogin (data) {
+  // to be added later
+  return true
+}
+
+
+
+
+function validateNewURL (data) {
+  // to be added later
+  return true
+}
