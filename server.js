49246181c85a2aa5ae03d4351fec8f4ad5e4fed1
Rayhaneh Banyassady <r.b.assady@gmail.com>
2017-08-20 17:07:46 +0000

    Added some comments and removed unnecassary comments.

--- server.js
+++ server.js
@@ -1,34 +1,35 @@
-"use strict";
+'use strict'
 
 require('dotenv').config();
 
-const PORT        = process.env.PORT || 8080;
-const ENV         = process.env.ENV || "development";
-const express     = require("express");
-const bodyParser  = require("body-parser");
-const sass        = require("node-sass-middleware");
-const app         = express();
-const cookieSession  = require('cookie-session')
+const PORT          = process.env.PORT || 8080;
+const ENV           = process.env.ENV || "development";
+const express       = require("express");
+const bodyParser    = require("body-parser");
+const sass          = require("node-sass-middleware");
+const app           = express();
+const cookieSession = require('cookie-session')
 
 
-const knexConfig  = require("./knexfile");
-const knex        = require("knex")(knexConfig[ENV]);
-const morgan      = require('morgan');
-const knexLogger  = require('knex-logger');
+const knexConfig    = require("./knexfile");
+const knex          = require("knex")(knexConfig[ENV]);
+const morgan        = require('morgan');
+const knexLogger    = require('knex-logger');
+
 
 // Seperated Routes for each Resource
-const usersRoutes = require("./routes/users");
-const logsRoutes  = require("./routes/logs");
-const urlsRoutes  = require("./routes/urls");
+const usersRoutes   = require("./routes/users");
+const logsRoutes    = require("./routes/logs");
+const urlsRoutes    = require("./routes/urls");
+
 
 // Load the logger first so all (static) HTTP requests are logged to STDOUT
-// 'dev' = Concise output colored by response status for development use.
-//         The :status token will be colored red for server error codes, yellow for client error codes, cyan for redirection codes, and uncolored for all other codes.
 app.use(morgan('dev'));
 
 // Log knex SQL queries to STDOUT as well
 app.use(knexLogger(knex));
 
+// Middlewares
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use("/styles", sass({
@@ -49,7 +50,7 @@ const urlDataHelpers  = require('./db/data_helpers/url_data_helpers.js')(knex)
 const userDataHelpers = require('./db/data_helpers/user_data_helpers.js')(knex)
 
 
-// USER AUTHENTICATION
+// USER AUTHENTICATION MIDDLEWARE
 app.use((req, res, next) => {
   // If there is a cookie attach the current user's info to the request object
   if (req.session.user_id) {
@@ -78,15 +79,11 @@ app.use((req, res, next) => {
 
 
 // Mount all resource routes
-//app.use("/api/users", usersRoutes(knex));
 app.use("/users", usersRoutes(userDataHelpers));
 app.use("/", logsRoutes(userDataHelpers))
 app.use("/urls", urlsRoutes(urlDataHelpers))
 
-// Home page
-// app.get("/", (req, res) => {
-//   res.render('index', {'currentUser': req.currentUser});
-// })
+// Redirect home page to /urls
 app.get("/", (req, res) => {
   res.redirect("/urls")
 })
