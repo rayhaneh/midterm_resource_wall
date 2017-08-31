02b574a0bc90eb24b0d19e37d8229be96fd4a559
Rayhaneh Banyassady <r.b.assady@gmail.com>
2017-08-31 02:42:13 +0000

    Heroku debugging

--- knexfile.js
+++ knexfile.js
@@ -2,16 +2,26 @@ require('dotenv').config();
 
 module.exports = {
 
+  development: {
+    client: 'postgresql',
+    connection: {
+      host     : process.env.DB_HOST,
+      user     : process.env.DB_USER,
+      password : process.env.DB_PASS,
+      database : process.env.DB_NAME,
+      port     : process.env.DB_PORT,
+      ssl      : process.env.DB_SSL
+    },
+    migrations: {
+      directory: './db/migrations',
+      tableName: 'migrations'
+    },
+    seeds: {
+      directory: './db/seeds'
+    }
+  },
+
   production: {
-    // client: 'postgresql',
-    // connection: {
-    //   host     : process.env.DB_HOST,
-    //   user     : process.env.DB_USER,
-    //   password : process.env.DB_PASS,
-    //   database : process.env.DB_NAME,
-    //   port     : process.env.DB_PORT,
-    //   ssl      : process.env.DB_SSL
-    // },
     connection: process.env.DB_URL + '?ssl=true',
     migrations: {
       directory: './db/migrations',
@@ -22,20 +32,20 @@ module.exports = {
     }
   },
 
-  development: {
-    client: 'postgresql',
-    connection: process.env.DB_URL + '?ssl=true',
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      directory: './db/migrations',
-      tableName: 'migrations'
-    },
-    seeds: {
-      directory: './db/seeds'
-    }
-  }
+  // production: {
+  //   client: 'postgresql',
+  //   connection: process.env.DB_URL + '?ssl=true',
+  //   pool: {
+  //     min: 2,
+  //     max: 10
+  //   },
+  //   migrations: {
+  //     directory: './db/migrations',
+  //     tableName: 'migrations'
+  //   },
+  //   seeds: {
+  //     directory: './db/seeds'
+  //   }
+  // }
 
 };
