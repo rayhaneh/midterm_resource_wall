2d6d1fd09011d0ac308ad0b78bf5a02c74502cf6
Rayhaneh Banyassady <r.b.assady@gmail.com>
2017-08-31 02:40:48 +0000

    Heroku debugging

--- knexfile.js
+++ knexfile.js
@@ -2,17 +2,17 @@ require('dotenv').config();
 
 module.exports = {
 
-  development: {
-    client: 'postgresql',
-    connection: {
-      host     : process.env.DB_HOST,
-      user     : process.env.DB_USER,
-      password : process.env.DB_PASS,
-      database : process.env.DB_NAME,
-      port     : process.env.DB_PORT,
-      ssl      : process.env.DB_SSL
-    },
-    // connection: process.env.DB_URL + '?ssl=true',
+  production: {
+    // client: 'postgresql',
+    // connection: {
+    //   host     : process.env.DB_HOST,
+    //   user     : process.env.DB_USER,
+    //   password : process.env.DB_PASS,
+    //   database : process.env.DB_NAME,
+    //   port     : process.env.DB_PORT,
+    //   ssl      : process.env.DB_SSL
+    // },
+    connection: process.env.DB_URL + '?ssl=true',
     migrations: {
       directory: './db/migrations',
       tableName: 'migrations'
@@ -22,9 +22,13 @@ module.exports = {
     }
   },
 
-  production: {
+  development: {
     client: 'postgresql',
     connection: process.env.DB_URL + '?ssl=true',
+    pool: {
+      min: 2,
+      max: 10
+    },
     migrations: {
       directory: './db/migrations',
       tableName: 'migrations'
