89b47fb97ff81aea0ddd30c51d0944534efc6bba
Rayhaneh Banyassady <r.b.assady@gmail.com>
2017-08-31 02:38:53 +0000

    Heroku debugging

--- knexfile.js
+++ knexfile.js
@@ -25,10 +25,6 @@ module.exports = {
   production: {
     client: 'postgresql',
     connection: process.env.DB_URL + '?ssl=true',
-    pool: {
-      min: 2,
-      max: 10
-    },
     migrations: {
       directory: './db/migrations',
       tableName: 'migrations'
