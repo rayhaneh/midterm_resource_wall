259ba418efc426cf83d3f5c93efe1dbdd154d30c
Rayhaneh Banyassady <r.b.assady@gmail.com>
2017-08-31 03:00:46 +0000

    Changes the knexfile for heroku

--- knexfile.js
+++ knexfile.js
@@ -33,20 +33,4 @@ module.exports = {
   //   }
   // },
 
-  development: {
-    client: 'postgresql',
-    connection: process.env.DB_URL + '?ssl=true',
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      directory: './db/migrations',
-      tableName: 'migrations'
-    },
-    seeds: {
-      directory: './db/seeds'
-    }
-  }
-
 };
